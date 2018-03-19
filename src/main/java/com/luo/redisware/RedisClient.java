18c6c75c741810de9b18b34a0d3b11033e9e7214
luoxn28 <luoxn28@163.com>
2018-03-19 19:42:12 +0800

    init

---
+++ src/main/java/com/luo/redisware/RedisClient.java
@@ -0,0 +1,16 @@
+package com.luo.redisware;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * Redis客户端
+ * @author xiangnan
+ * @date 2018/3/19 11:47
+ */
+public class RedisClient {
+    private static Logger logger = LogManager.getLogger(RedisClient.class);
+
+
+
+}
---
+++ src/test/java/com/test/redisware/RedisTest.java
@@ -0,0 +1,8 @@
+package com.test.redisware;
+
+/**
+ * @author xiangnan
+ * @date 2018/3/19 19:41
+ */
+public class RedisTest {
+}
